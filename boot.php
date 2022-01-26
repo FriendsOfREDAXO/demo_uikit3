9358c1cfcec079040f784661c1d47f6e810afe3d
Thomas Skerbis <thomas.skerbis@mac.com>
2022-01-26 17:38:53 +0100

    uikit assets change to dist folder

--- boot.php
+++ boot.php
@@ -1,9 +1,9 @@
 <?php
 // UIKit3 for backend
 if (rex::isBackend() && rex::getUser()) {
-        rex_view::addCssFile($this->getAssetsUrl('demo_uikit3/css/uikit.min.css'));
-        rex_view::addJsFile($this->getAssetsUrl('demo_uikit3/js/uikit.min.js'));
-        rex_view::addJsFile($this->getAssetsUrl('demo_uikit3/js/uikit-icons.min.js'));
+        rex_view::addCssFile($this->getAssetsUrl('uikit/dist/css/uikit.min.css'));
+        rex_view::addJsFile($this->getAssetsUrl('uikit/dist/js/uikit.min.js'));
+        rex_view::addJsFile($this->getAssetsUrl('uikit/dist/js/uikit-icons.min.js'));
 }
 // Some helper functions
 rex_extension::register('PACKAGES_INCLUDED', function($ep) {
