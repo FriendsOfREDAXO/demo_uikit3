a861d80613c5298704f54797dce8a24aa68697cc
Thomas Skerbis <thomas.skerbis@mac.com>
2022-01-26 17:25:54 +0100

    Adding Assets

--- boot.php
+++ boot.php
@@ -1,5 +1,11 @@
 <?php
+// UIKit3 for backend
+if (rex::isBackend() && rex::getUser()) {
+        rex_view::addCssFile($this->getAssetsUrl('demo_uikit3/css/uikit.min.css'));
+        rex_view::addJsFile($this->getAssetsUrl('demo_uikit3/js/uikit.min.js'));
+        rex_view::addJsFile($this->getAssetsUrl('demo_uikit3/js/uikit-icons.min.js'));
+}
+// Some helper functions
 rex_extension::register('PACKAGES_INCLUDED', function($ep) {
         include_once rex_path::addon('demo_uikit3/inc').'helper_func.php';
 });
- 
